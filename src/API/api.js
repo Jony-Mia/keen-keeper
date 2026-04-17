ec8363e42feacce7618de98888d657ee7ac19421
Jony-Mia <ajonymia321@gmail.com>
2026-04-17 23:23:15 +0600

    stop caching

--- src/API/api.js
+++ src/API/api.js
@@ -1,11 +1,7 @@
-import axios from "axios";
 export const getFriends = async () => {
-    const res = await fetch("http://localhost:3000/friends.json")
+    const res = await fetch("http://localhost:3000/friends.json",{
+        cache:"no-store"
+    })
     let data = await res.json();
     return data;
 }
-
-// export const getFriends =  async () => {
-//     const res = await axios("/friends.json")
-//     return await res.data;
-// }
